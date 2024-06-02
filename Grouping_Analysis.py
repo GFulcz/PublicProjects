a3ed98b2a4e360e9f761625cd01bcde781ff7bb0
Venomm321 <gfulczyk@gmail.com>
2024-06-03 00:26:04 +0200

    Grouping_Analysis update
    Update of one of the scrips in repository.
    
    - Grouping_Analysis.py replaces Data_Checker 3.py
    - Grouping_Analysis includes small code corrections - renaming variables, optimizing flow of variables
    - Created diagram for main function of Grouping_Analysis.py
    - Updated README.md file

--- Grouping_Analysis.py
+++ Grouping_Analysis.py
@@ -1,55 +1,3 @@
-'''
-FAQ
-- What does this do? 
-This program allows to group selected dataset by categories, analyse it and then retrieve this data back into original file structure.
-Grouping can be performed by Categorical or Numerical values. Please note that resulting values are only numerical.
-User can also import Perform_DataFrame_Analysis() function to other scrips.
-
-- What do the Grouping columns mean?
-This is a list of columns inside the analysed file, selected by the user to perform grouping and further analysis. This can be either a singular column, or multiple ones.
-When defining this, it's recommended to select columns representing categorical information. 
-If a column containing very specific data is selected, such as indexing info - Client Numbers, Item id etc. then the program will yield results identical from original file.
-
-- Why would I want to Exclude columns?
-Some data uses Numerical values for describing categories. For example calculating average results from this column would not yield any useful info. 
-In this case, its recommended to exclude this column.
-
-- What do the Analysistype options mean?
-This selects which type of analysis is performed by program. User can select from:
-'max', 'min', 'sum', 'mean', 'median', 'std'
-
-- What do the Resultstype options mean?
-This selectes which type of results are returned back. User can select from: 
-'grouped', 'relative_grouped', 'both' - when using terminal interface program.
-'grouped', 'relative_grouped' - when using separate Perform_DataFrame_Analysis() function after importing it.
-
-- What for can I use Resultstype = grouped?
-This can be used to compare original values to the direct results of grouping and analysis.
-This can be helpful to use direct mean values of every category to fill NaN values in a given dataset.
-
-- What for can I use Resultstype = grouped_relative?
-This can be used to directly inspect a ratio of a given original value to the calculated grouping value.
-This can be helpful to quickly generate a lot of comparative data within a given dataset.
-
-- I am using Resultstype = grouped_relative argument, my resulting dataset contains inf values, why?
-This occurs when grouping result for a given column is equal to zero. When defining a ratio of original value to grouped value a division by zero occurs.
-In this case, program defines the ratio value to be inf.
-
-- I see that some of my columns have been excluded from analysis, despite my original input, why?
-This could have happend due to Exclude_recognised_boolean_columns = True. This allows program to select numerical columns containing only [0,1] values across the whole table.
-This is considered to be a Boolean type data and is automatically excluded by program.
-You can disable this manually when using imported Perform_DataFrame_Analysis() function.
-
-- What does Grouping_dropna argument change?
-This is a pandas functionality of pandas.DataFrame.groupby(dropna=False).
-If it is enabled then NA values together with row/column will be dropped during grouping. If false, NA values will be treated as the key in groups.
-You can enable this manually when using imported Perform_DataFrame_Analysis() function.
-
-- I see that inside this file there are many #comments or #TODO notes, why?
-This file is a result of a private work on a spare time, partly as a hobby, the TODO notes are my own notes for things that in my opinion should be implemeted/fixed at a later date.
-In case of suggestions or comments, I invite you to please get in contact on github.
-
-'''
 
 import pandas as pd
 from math import inf
@@ -113,7 +61,7 @@ def Perform_DataFrame_Analysis(User_DataFrame: pd.DataFrame, Grouping_columns: [
     # Grouping_columns: [str] = ['sex', 'sibsp']
     # Exclude_columns: [str] = []
     # Analysistype: str = 'mean'
-    # Resultstype: str = 'both' # 'both or 'grouped' 'relative_grouped'
+    # Resultstype: str = 'both' # 'both or 'fill_with_grouped' 'calculate_relative_grouped'
 
     # # TODO analysis type 'all'
     # # for analysis type "all" add logic to exclude earlier analysed columns 
@@ -144,15 +92,15 @@ def Perform_DataFrame_Analysis(User_DataFrame: pd.DataFrame, Grouping_columns: [
 
 
     Analysistype_valid = ['max', 'min', 'sum', 'mean', 'median', 'std', 'Confidence_interval']
-    Resultstype_valid = ['grouped', 'relative_grouped']
+    Resultstype_valid = ['fill_with_grouped', 'calculate_relative_grouped']
 
-    Dataframe = User_DataFrame
+    
 
     if Exclude_recognised_boolean_columns:
-        boolean_columns = List_detected_boolean_columns(Dataframe)
+        boolean_columns = List_detected_boolean_columns(User_DataFrame)
         Exclude_columns += boolean_columns
 
-    Groups = Dataframe.groupby(by=Grouping_columns, dropna=Grouping_dropna)
+    Groups = User_DataFrame.groupby(by=Grouping_columns, dropna=Grouping_dropna)
 
     match Analysistype:
         case 'max':
@@ -175,15 +123,16 @@ def Perform_DataFrame_Analysis(User_DataFrame: pd.DataFrame, Grouping_columns: [
             raise ValueError(f'Analysistype: Analysistype must be one of {Analysistype_valid}') 
 
 
-    Dataframe_grouped = pd.DataFrame(data=None, columns=Dataframe.columns)
-    Dataframe_compared = pd.DataFrame(data=None, columns=Dataframe.columns)
+    
+    
 
 
     match Resultstype:
-        case 'grouped':
-            df_range = range(Dataframe.shape[0])
+        case 'fill_with_grouped':
+            df_range = range(User_DataFrame.shape[0])
+            Dataframe_grouped = pd.DataFrame(data=None, columns=User_DataFrame.columns)
             for i, row in enumerate(df_range):
-                original_row = Dataframe.iloc[row]
+                original_row = User_DataFrame.iloc[row]
                 original_row_dict = original_row.to_dict()
                 original_row_index = tuple(original_row[Grouping_columns].values)
                 grouped_row_dict = Grouped_results.loc[original_row_index].to_dict()
@@ -194,10 +143,11 @@ def Perform_DataFrame_Analysis(User_DataFrame: pd.DataFrame, Grouping_columns: [
                 Dataframe_grouped.loc[last_index] = grouped_values_dict
 
 
-        case 'relative_grouped':
-            df_range = range(Dataframe.shape[0])
+        case 'calculate_relative_grouped':
+            df_range = range(User_DataFrame.shape[0])
+            Dataframe_compared = pd.DataFrame(data=None, columns=User_DataFrame.columns)
             for i, row in enumerate(df_range):
-                original_row = Dataframe.iloc[row]
+                original_row = User_DataFrame.iloc[row]
                 original_row_dict = original_row.to_dict()
                 original_row_index = tuple(original_row[Grouping_columns].values)
                 grouped_row_dict = Grouped_results.loc[original_row_index].to_dict()
@@ -244,7 +194,7 @@ def select_file() -> str:
 if __name__ == '__main__':
     filetype_valid = ['.csv', '.xls', '.xlsx']
     Analysistype_valid = ['max', 'min', 'sum', 'mean', 'median', 'std', 'Confidence_interval']
-    Resultstype_valid = ['grouped', 'relative_grouped', 'both']
+    Resultstype_valid = ['fill_with_grouped', 'calculate_relative_grouped', 'both']
 
 
     print("Please select a file to analyse")
@@ -282,6 +232,8 @@ if __name__ == '__main__':
             print(f'Column "{col}" not recognised in the original file. Please try again\n')
 
 
+
+    #TODO
     # while iter < limit:
     #     grouping = str(input('Please select columns for grouping from the above, separated by a "," comma:\n'))
     #     grouping = grouping.split(sep=',')
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# PublicProjects
-Repository destined for projects published publicly
+# Grouping_Analysis
+
+Grouping_Analysis is a Python script and function for grouping data within a given dataset and calculating simple reference values to their grouped characteristics.
+
+## Installation
+
+Grouping_Analysis is a single file Python script and doesn't require explicit installation. 
+Script uses [pandas](https://pypi.org/project/pandas/) library.
+
+```bash
+pip install pandas
+```
+
+## FAQ
+
+- What does this do?
+
+This program allows to group selected dataset by categories, analyse it and then retrieve this data back into original file structure.
+Grouping can be performed by Categorical or Numerical values. Please note that resulting values are only numerical.
+User can also import Perform_DataFrame_Analysis() function to other scrips.
+
+- What do the Grouping columns mean?
+
+This is a list of columns inside the analysed file, selected by the user to perform grouping and further analysis. This can be either a singular column, or multiple ones.
+When defining this, it's recommended to select columns representing categorical information. 
+If a column containing very specific data is selected, such as indexing info - Client Numbers, Item id etc. then the program will yield results identical from original file.
+
+- Why would I want to Exclude columns?
+
+Some data uses Numerical values for describing categories. For example calculating average results from this column would not yield any useful info. 
+In this case, its recommended to exclude this column.
+
+- What do the Analysistype options mean?
+
+This selects which type of analysis is performed by program. User can select from:
+'max', 'min', 'sum', 'mean', 'median', 'std'
+
+- What do the Resultstype options mean?
+
+This selectes which type of results are returned back. User can select from: 
+'grouped', 'relative_grouped', 'both' - when using terminal interface program.
+'grouped', 'relative_grouped' - when using separate Perform_DataFrame_Analysis() function after importing it.
+
+- What for can I use Resultstype = grouped?
+
+This can be used to compare original values to the direct results of grouping and analysis.
+This can be helpful to use direct mean values of every category to fill NaN values in a given dataset.
+
+- What for can I use Resultstype = grouped_relative?
+
+This can be used to directly inspect a ratio of a given original value to the calculated grouping value.
+This can be helpful to quickly generate a lot of comparative data within a given dataset.
+
+- I am using Resultstype = grouped_relative argument, my resulting dataset contains inf values, why?
+
+This occurs when grouping result for a given column is equal to zero. When defining a ratio of original value to grouped value a division by zero occurs.
+In this case, program defines the ratio value to be inf.
+
+- I see that some of my columns have been excluded from analysis, despite my original input, why?
+
+This could have happend due to Exclude_recognised_boolean_columns = True. This allows program to select numerical columns containing only [0,1] values across the whole table.
+This is considered to be a Boolean type data and is automatically excluded by program.
+You can disable this manually when using imported Perform_DataFrame_Analysis() function.
+
+- What does Grouping_dropna argument change?
+
+This is a pandas functionality of pandas.DataFrame.groupby(dropna=False).
+If it is enabled then NA values together with row/column will be dropped during grouping. If false, NA values will be treated as the key in groups.
+You can enable this manually when using imported Perform_DataFrame_Analysis() function.
+
+- I see that inside this file there are many #comments or #TODO notes, why?
+
+This file is a result of a private work on a spare time, partly as a hobby, the TODO notes are my own notes for things that in my opinion should be implemeted/fixed at a later date.
+In case of suggestions or comments, I invite you to please get in contact on github.
