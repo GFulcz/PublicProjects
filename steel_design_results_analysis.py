f07fe391b4e5b0b9699c691bdbfe9aa072dae7c8
Venomm321 <gfulczyk@gmail.com>
2024-06-06 23:19:45 +0200

    steel_design_results_analysis update
    update of steel_design_results_analysis.py
    
    - fixed naming conventions withing script - not finished, in progress TODO
    - updated commentary for code segments representing operations on Database
    - opimized calculation of Stability check ratio to max Design check ratio of every member in db

--- steel_design_results_analysis.py
+++ steel_design_results_analysis.py
@@ -6,39 +6,36 @@ import sqlite3
 # https://stackoverflow.com/questions/70937038/group-by-works-with-select-columns-not-in-group-by
 import numpy as np
 
-
 def select_file() -> str:
     '''
     Function selecting windows directory with .csv files exported from the program.
     '''
     file_path = filedialog.askdirectory(title="Select the directory with exported .csv files")
-    if file_path:
-        print("Selected path:", file_path, '\n')
-    else:
-        print("No path selected.\n")
-
+    assert len(file_path) != 0, 'No file was chosen'
     return file_path
-   
+
 
 def parse_range_string(range_string):
-    result = []
+    '''
+    Function changes the comma and dash notation of a range to a list of elements
+    '''
+    items = []
     ranges = range_string.split(',')
 
     for r in ranges:
         if '-' in r:
             start = int(r.split('-')[0])
             end = int(r.split('-')[1])
-            result.extend(range(start, end + 1))
+            items.extend(range(start, end + 1))
         else:
-            result.append(int(r))
+            items.append(int(r))
 
-    return result
+    return items
 
 if __name__=='__main__':
 
     print('Please select a directory with .csv files exported')
     dir_path = select_file()
-    # dir_path = r"C:\\Users\\Grzegorz\Documents\\GitHub\\Test-repo-GF\\GF\\0__OwnRepo\Statistical\Stahlhalle"
     files_list = os.listdir(dir_path)
     member_results_file: str = None
     member_sections_file: str = None
@@ -48,11 +45,11 @@ if __name__=='__main__':
         if item == "Sections.csv":
             member_sections_file = item
 
-    # Create SQLite db
+    # Create SQLite db or connect to an existing one
     db = sqlite3.connect('RFEM_results.db')
     cur = db.cursor()
 
-
+    # Purge existing DB data
     cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
     if len(cur.fetchall()) > 1:
         cur.execute('DROP TABLE IF EXISTS Steel_Design')
@@ -63,146 +60,166 @@ if __name__=='__main__':
 
     # TODO add option for member sets
     # Retrieve clear data from Steel_Design CSV file
+    member_results_path: str = '{}{}{}'.format(dir_path, os.sep, member_results_file)
+    with open(member_results_path, 'r', encoding='utf-8-sig') as f:
+        results = csv.reader(f, delimiter=";")
 
-    # if True segment used to create collapse/expand code sections
-    if True:
-        member_results_path: str = '{}{}{}'.format(dir_path, os.sep, member_results_file)
-        with open(member_results_path, 'r', encoding='utf-8-sig') as f:
-            results = csv.reader(f, delimiter=";")
+        # Get content of headlines
+        headline1 = next(results)
+        headline2 = next(results)
 
-            headline1 = next(results)
-            headline2 = next(results)
+        # Rename content of headlines to custom column titles
+        col0_Member_no = headline1[0].replace(' ','_').replace('.','')
+        col1_Location_x = f'{headline1[1]}_{headline2[1].split(" ")[0]}'
+        col2_Stress_Point_no = f"{headline1[2]}_{headline2[2].replace(' ','_').replace('.','')}"
+        col3_Design_Situation_name = f'{headline1[3]}_{headline2[3]}'
+        col4_Load_Combination_no = f"{headline1[4]}_{headline2[4].replace('.','')}"
+        col5_Design_Check_ratio = f"{headline1[5].replace(' ','_')}_{headline2[5].split(' ')[0]}"
+        col6_Design_Check_type = f"{headline1[5].replace(' ','_')}_{headline2[6]}"
+        col7_Design_Check_description = f"{headline1[5].replace(' ','_')}_{headline2[7]}"
 
-
-            col0_Member_no = headline1[0].replace(' ','_').replace('.','')
-            col1_Location_x = f'{headline1[1]}_{headline2[1].split(" ")[0]}'
-            col2_Stress_Point_no = f"{headline1[2]}_{headline2[2].replace(' ','_').replace('.','')}"
-            col3_Design_Situation_name = f'{headline1[3]}_{headline2[3]}'
-            col4_Load_Combination_no = f"{headline1[4]}_{headline2[4].replace('.','')}"
-            col5_Design_Check_ratio = f"{headline1[5].replace(' ','_')}_{headline2[5].split(' ')[0]}"
-            col6_Design_Check_type = f"{headline1[5].replace(' ','_')}_{headline2[6]}"
-            col7_Design_Check_description = f"{headline1[5].replace(' ','_')}_{headline2[7]}"
-
-            headlines: [str] = [col0_Member_no,
-                                col1_Location_x,
-                                col2_Stress_Point_no,
-                                col3_Design_Situation_name,
-                                col4_Load_Combination_no,
-                                col5_Design_Check_ratio,
-                                col6_Design_Check_type,
-                                col7_Design_Check_description]
+        headlines: list[str] = [col0_Member_no,
+                            col1_Location_x,
+                            col2_Stress_Point_no,
+                            col3_Design_Situation_name,
+                            col4_Load_Combination_no,
+                            col5_Design_Check_ratio,
+                            col6_Design_Check_type,
+                            col7_Design_Check_description]
 
 
-
-            data: [[str]] = []
-
-            members_total = 0
-            for i, line in enumerate(results):
-
-                if line[1].find('|') >= 0:
-                    continue
-                if len(line[1]) == 0:
-                    continue
-
-                members_total = int(line[0])
-                data.append((line))
+        # Skip non data rows and retrieve data
+        data: [[str]] = []
+        members_total = 0
+        for i, line in enumerate(results):
+            if line[1].find('|') >= 0:
+                continue
+            if len(line[1]) == 0:
+                continue
+            members_total = int(line[0])
+            data.append((line))
 
 
-    # Create and populate Steel_Design Table
-    if True:
-        table_columns: str = ''
-        for col in headlines:
-            table_columns += f'{col} TEXT, \n'
-        table_columns = table_columns[:-3]
-
-        sql: str = f'''
-            CREATE TABLE Steel_Design (
-                result_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                {table_columns}
-                ) ;'''
-
-        cur.execute(sql)
+    # Create Steel_Design Table
+    table_columns: str = ''
+    for col in headlines:
+        table_columns += f'{col} TEXT, \n'
+    table_columns = table_columns[:-3]
+    sql: str = f'''
+        CREATE TABLE Steel_Design (
+            result_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            {table_columns}
+            ) ;'''
+    cur.execute(sql)
 
 
-        table_columns: str = ''
-        for col in headlines:
-            table_columns += f'{col}, \n'
-        table_columns = table_columns[:-3]
-
-        sql = f'''INSERT INTO Steel_Design ({table_columns})
-                    VALUES (?,?,?,?,?,?,?,?);'''
-        cur.executemany(sql, data)
-
-
-    # Create Contains_warning column and delete non numeric ratios
-    if True:
-        cur.execute("SELECT DISTINCT Design_Check_Ratio FROM Steel_Design ORDER BY Design_Check_Ratio DESC;")
-
-        non_numeric_ratio_val = []
-        for result in cur:
-            result_value = result[0].replace('.','')
-            if result_value.isnumeric() == False:
-                non_numeric_ratio_val.append(result_value)
+    # Populate Steel_Design table
+    table_columns: str = ''
+    for col in headlines:
+        table_columns += f'{col}, \n'
+    table_columns = table_columns[:-3]
+    sql = f'''INSERT INTO Steel_Design ({table_columns})
+                VALUES (?,?,?,?,?,?,?,?);'''
+    cur.executemany(sql, data)
 
 
-        sql_non_numeric_values:str = ''
-        for item in non_numeric_ratio_val:
-            sql_non_numeric_values += f" \'{item}\',"
-        sql_non_numeric_values= sql_non_numeric_values[:-1]
+    # Distinguish values of non-numeric results in Design_Check_Ratio columns
+    cur.execute("SELECT DISTINCT Design_Check_Ratio FROM Steel_Design ORDER BY Design_Check_Ratio DESC;")
+    non_numeric_ratio_val = []
+    for result in cur:
+        result_value = result[0].replace('.','')
+        if result_value.isnumeric() is False:
+            non_numeric_ratio_val.append(result_value)
 
 
-        cur.execute(f"SELECT DISTINCT Member_No FROM Steel_Design WHERE Design_Check_Ratio IN ({sql_non_numeric_values});")
-        members_containing_warning_or_error = []
-        for i, result in enumerate(cur):
-            members_containing_warning_or_error.append((result))
+    # Create a list containing strings of non-numeric results
+    sql_non_numeric_values:str = ''
+    for item in non_numeric_ratio_val:
+        sql_non_numeric_values += f" \'{item}\',"
+    sql_non_numeric_values= sql_non_numeric_values[:-1]
 
 
-        cur.execute('ALTER TABLE Steel_Design ADD Contains_Warning_or_Error INTEGER DEFAULT 0')
+    # Create a list of Member Numbers of Members having non-numeric results
+    cur.execute(f"SELECT DISTINCT Member_No FROM Steel_Design WHERE Design_Check_Ratio IN ({sql_non_numeric_values});")
+    members_containing_warning_or_error = []
+    for i, result in enumerate(cur):
+        members_containing_warning_or_error.append((result))
 
 
-        sql: str = f'''
-            UPDATE Steel_Design SET Contains_Warning_or_Error = 1
-                WHERE Member_NO = ?'''
-        cur.executemany(sql, members_containing_warning_or_error)
+    # Add a new column representing if a Member has non-numeric results or not
+    cur.execute('ALTER TABLE Steel_Design ADD Contains_Warning_or_Error INTEGER DEFAULT 0')
 
 
-        cur.execute(f'DELETE FROM Steel_Design WHERE DESIGN_CHECK_RATIO IN ({sql_non_numeric_values});')
+    # Add a value of 1 (True) for Members from a prepared list
+    sql: str = ''' UPDATE Steel_Design 
+                    SET Contains_Warning_or_Error = 1
+                        WHERE Member_NO = ?'''
+    cur.executemany(sql, members_containing_warning_or_error)
 
 
-    # Create ratio_max_des_to_stability_des and fillout calculated ratio
-    if True:
-        cur.execute('ALTER TABLE Steel_Design ADD Ratio_max_Design_Check_to_Stability INTEGER DEFAULT 0')
-
-        cur.execute('SELECT Member_No, max(cast(Design_Check_Ratio as real)), Design_Check_Description FROM Steel_Design WHERE Design_Check_Description LIKE "Stability%" GROUP BY Member_No ORDER BY cast(Member_No as integer)')
-        member_stability_max: [[]] = []
-        for result in cur:
-            result_list = [(result[0]), float(result[1])]
-            member_stability_max.append(result_list)
+    # Delete rows which have non-numeric values in Design_Check_Ratio column
+    cur.execute(f'DELETE FROM Steel_Design WHERE Design_Check_Ratio IN ({sql_non_numeric_values});')
 
 
-        cur.execute('SELECT Member_No, max(cast(Design_Check_Ratio as real)) FROM Steel_Design GROUP BY Member_No ORDER BY cast(Member_No as integer)')
-        member_ratio_max: [[]]  = []
-        for result in cur:
-            result_list = [(result[0]), float(result[1])]
-            member_ratio_max.append(result_list)
-            # print(result)
 
-        ratios_list = []
-        for max_list in member_ratio_max:
-            for stability_list in member_stability_max:
-                if max_list[0] == stability_list[0]:
-                    if max_list[1] == 0:
-                        max_list[1] = 0.001
 
-                    ratios_list.append([round( (stability_list[1]/max_list[1]) , 3), max_list[0]])
-                    # print(f'{[max_list[0]]}, {round( (stability_list[1]/max_list[1]) , 3)}')
-                    # print(f'{max_list[0]} {max_list[1]} _ {stability_list[0]} {stability_list[1]}: {round((stability_list[1] /max_list[1]),2) }')
-                    # break
 
-        sql: str = f'''
-            UPDATE Steel_Design SET Ratio_max_Design_Check_to_Stability = ?
-                WHERE Member_NO = ?'''
-        cur.executemany(sql, ratios_list)
+
+    # Create a list of members and their maximal value of Design_Check_Ratio
+    cur.execute('''SELECT Member_No, max(cast(Design_Check_Ratio as real)) FROM Steel_Design
+                    GROUP BY Member_No ORDER BY cast(Member_No as integer)''')
+    member_ratio_max: [[]]  = []
+    for result in cur:
+        result_list = [(result[0]), float(result[1])]
+        member_ratio_max.append(result_list)
+
+
+    # Create a list of members which maximal value of Design_Check_Ratio is caused by Stability check
+    cur.execute('''SELECT Member_No, max(cast(Design_Check_Ratio as real)), Design_Check_Description
+                    FROM Steel_Design WHERE Design_Check_Description LIKE "Stability%" 
+                    GROUP BY Member_No ORDER BY cast(Member_No as integer)''')
+    member_stability_max: list[list] = []
+    for result in cur:
+        result_list = [(result[0]), float(result[1])]
+        member_stability_max.append(result_list)
+
+
+    # Add a new column representing if a Member has a max Design Check value caused by Stability check
+    cur.execute('ALTER TABLE Steel_Design ADD Ratio_max_Design_Check_to_Stability INTEGER DEFAULT 0')
+
+
+
+    # Calculate a ratio of Stability check value to max Design check value for every member
+    ratios_list = []
+    innerloop_range = len(member_stability_max)
+    innerloop_maxval = member_stability_max[len(member_stability_max)-1]
+    startpoint = 0
+    for max_list in member_ratio_max:
+        if int(max_list[0]) > int(innerloop_maxval[0]):
+            # cannot use break statement, because in case of repeating datasets
+            # some of the data will be ignored
+            continue
+
+        for j in range(startpoint, innerloop_range, 1):
+            stability_list = member_stability_max[j]
+
+            if stability_list[0] > max_list[0]:
+                break
+
+            if max_list[0] == stability_list[0]:
+                if max_list[1] == 0:
+                    max_list[1] = 0.001
+
+                ratios_list.append([round( (stability_list[1]/max_list[1]) , 3), max_list[0]])
+                startpoint = j+1
+                break
+
+
+    # Add a value of 1 (True) for Members from a prepared list
+    sql: str = ''' UPDATE Steel_Design 
+                    SET Ratio_max_Design_Check_to_Stability = ?
+                    WHERE Member_NO = ?'''
+    cur.executemany(sql, ratios_list)
 
 
 
